2d1e404437599e57ea4952837a1710d00bdf36a7
sunqi <1904705132@qq.com>
2022-02-17 14:35:41 +0800

    add_dfs

---
+++ b_huawei_leetcode/52封闭岛屿的数量(leetcode1254).go
@@ -0,0 +1,40 @@
+package b_huawei_leetcode
+
+func closedIsland(grid [][]int) int {
+
+	m, n, res := len(grid), len(grid[0]), 0
+
+	for i := 0; i < m; i++ {
+		closedIslandDFS(grid, i, 0)
+		closedIslandDFS(grid, i, n-1)
+	}
+	
+	for j := 0; j < n; j++ {
+		closedIslandDFS(grid, 0, j)
+		closedIslandDFS(grid, m-1, j)
+	}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == 0 {
+				res++
+				closedIslandDFS(grid, i, j)
+			}
+		}
+	}
+	return res
+}
+
+func closedIslandDFS(grid [][]int, i, j int) {
+	m, n := len(grid), len(grid[0])
+	if i < 0 || j < 0 || i >= m || j >= n {
+		return
+	}
+	if grid[i][j] == 1 {
+		return
+	}
+	grid[i][j] = 1
+	closedIslandDFS(grid, i+1, j)
+	closedIslandDFS(grid, i-1, j)
+	closedIslandDFS(grid, i, j-1)
+	closedIslandDFS(grid, i, j+1)
+}
---
+++ b_huawei_leetcode/54岛屿的最大面积(leetcode695).go
@@ -0,0 +1,31 @@
+package b_huawei_leetcode
+
+func maxAreaOfIsland(grid [][]int) int {
+	m, n, res := len(grid), len(grid[0]), 0
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == 1 {
+				area := maxAreaOfIslandDFS(grid, i, j)
+				if area > res {
+					res = area
+				}
+			}
+		}
+	}
+	return res
+}
+
+func maxAreaOfIslandDFS(grid [][]int, i, j int) int {
+	m, n := len(grid), len(grid[0])
+	if i < 0 || j < 0 || i >= m || j >= n {
+		return 0
+	}
+	if grid[i][j] == 0 {
+		return 0
+	}
+	grid[i][j] = 0
+	return maxAreaOfIslandDFS(grid, i-1, j) +
+		maxAreaOfIslandDFS(grid, i+1, j) +
+		maxAreaOfIslandDFS(grid, i, j-1) +
+		maxAreaOfIslandDFS(grid, i, j+1) + 1
+}
