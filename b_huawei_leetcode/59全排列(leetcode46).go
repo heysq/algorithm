fbea2119456080b14c2f055111479c1210f899d8
sunqi <1904705132@qq.com>
2022-02-18 22:23:14 +0800

    bfs

---
+++ b_huawei_leetcode/59全排列(leetcode46).go
@@ -0,0 +1,35 @@
+package b_huawei_leetcode
+
+func permute(nums []int) [][]int {
+	var res [][]int
+	premuteHelper(nums, &[]int{}, &res)
+	return res
+}
+
+func premuteHelper(nums []int, track *[]int, res *[][]int) {
+	if len(*track) == len(nums) {
+		
+		a := make([]int, len(*track))
+		copy(a, *track)
+		*res = append(*res, a)
+		return
+	}
+
+	for i := 0; i < len(nums); i++ {
+		if intArrayContain(*track, nums[i]) {
+			continue
+		}
+		*track = append(*track, nums[i])
+		premuteHelper(nums, track, res)
+		*track = (*track)[:len(*track)-1]
+	}
+}
+
+func intArrayContain(arr []int, num int) bool {
+	for _, v := range arr {
+		if v == num {
+			return true
+		}
+	}
+	return false
+}
---
+++ b_huawei_leetcode/60N皇后问题(leetcode59)_test.go
@@ -0,0 +1,12 @@
+package b_huawei_leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_solveNQueens(t *testing.T) {
+	result := solveNQueens(100)
+	// fmt.Println(result)
+	fmt.Println(len(result))
+}
